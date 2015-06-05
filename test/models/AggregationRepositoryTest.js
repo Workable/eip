0f02e37cdb3199aea3c7088dda89e3655d6077c5
Nikolaos Dimos <dimos@workable.com>
2015-06-05 13:30:55 +0300

    Added entries in .gitIgnore and in .jshintrc to enable Promise and should

--- test/models/AggregationRepositoryTest.js
+++ test/models/AggregationRepositoryTest.js
@@ -1,8 +1,7 @@
 var fixtures = require('mongoose-fixtures'),
-  should = require('should'),
   sinon = require('sinon'),
   sandbox = sinon.sandbox.create(),
-  AggregationRepository = require('../../lib/models/AggregatorRepository')
+  AggregationRepository = require('../../lib/models/AggregationRepository')
   ;
 
 
@@ -17,7 +16,6 @@ describe('Test AggregationRepository Model', function () {
 
   it('should add a new event', function (done) {
     AggregationRepository.add("abc", {name: 'Darth Veider'}, function (err, savedModel) {
-      should(err).be.null;
       savedModel.correlationId.should.equal('abc');
       savedModel.events.pop().name.should.equal('Darth Veider');
       done();
@@ -25,12 +23,12 @@ describe('Test AggregationRepository Model', function () {
   });
 });
 
-function stubAggregationRepository(method) {
-  return sandbox.stub(AggregationRepository, method, function () {
-    return {
-      exec: function (func) {
-        func("Stub error " + method);
-      }
-    };
-  });
-}
+//function stubAggregationRepository(method) {
+//  return sandbox.stub(AggregationRepository, method, function () {
+//    return {
+//      exec: function (func) {
+//        func("Stub error " + method);
+//      }
+//    };
+//  });
+//}
