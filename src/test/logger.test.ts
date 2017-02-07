45363e8f9e06c44cdd98d0457687b0a3b5e8dfdf
Nikos Kostoulas <kostoulas@workable.com>
2017-02-07 16:43:38 +0200

    Add logger test.

---
+++ src/test/logger.test.ts
@@ -0,0 +1,17 @@
+
+import * as logger from '../lib/logger';
+import * as log4js from 'log4js';
+
+describe('Test logger', function () {
+  it('should inialize logger with name aggregator-eip', function () {
+    logger.init(null);
+    logger.getLogger().should.eql(log4js.getLogger('[aggregator-eip]'));
+  });
+
+  it('should initialize logger with given one', function () {
+    const log = log4js.getLogger('test');
+    logger.init(log);
+    logger.getLogger().should.eql(log);
+  });
+
+});
