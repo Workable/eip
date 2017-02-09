cc7dac46845e05bfb46735b31b15d42d97db908e
Nikos Kostoulas <kostoulas@workable.com>
2017-02-09 13:57:00 +0200

    Add mapper test.

---
+++ src/test/processors/mapper.test.ts
@@ -0,0 +1,26 @@
+import Mapper from '../../lib/processors/mapper';
+import * as sinon from 'sinon';
+const sandbox = sinon.sandbox.create();
+
+describe('Mapper', function () {
+  let mapper;
+  let mapFn;
+
+  beforeEach(function () {
+    mapFn = sandbox.stub();
+    mapper = new Mapper({ id: 'id', input: [mapFn], name: 'name', previous: null });
+  })
+
+  describe('process', function () {
+    it('should call map and return result', async function () {
+      mapFn.returns('transformed');
+
+      const result = await mapper.process('event');
+      result.should.be.eql('transformed');
+
+      mapFn.args.should.eql([
+        ['event']
+      ]);
+    });
+  });
+});
