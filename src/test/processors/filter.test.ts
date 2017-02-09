d0452cbdcc7dd075f9551f5037a8552613594a7d
Nikos Kostoulas <kostoulas@workable.com>
2017-02-09 14:44:17 +0200

    Add filter test

---
+++ src/test/processors/filter.test.ts
@@ -0,0 +1,37 @@
+import Filter from '../../lib/processors/filter';
+import * as sinon from 'sinon';
+import * as should from 'should';
+
+const sandbox = sinon.sandbox.create();
+
+describe('Filter', function () {
+  let filter;
+  let filterFn;
+
+  beforeEach(function () {
+    filterFn = sandbox.stub();
+    filter = new Filter({ id: 'id', input: [filterFn], name: 'name', previous: null });
+  })
+
+  describe('process', function () {
+    it('should call filter and return event', async function () {
+      filterFn.returns(true);
+
+      const result = await filter.process('event');
+      result.should.equal('event');
+      filterFn.args.should.eql([
+        ['event']
+      ]);
+    });
+
+    it('should call filter and return undefined', async function () {
+      filterFn.returns(false);
+
+      const result = await filter.process('event');
+      should.equal(undefined, result);
+      filterFn.args.should.eql([
+        ['event']
+      ]);
+    });
+  });
+});
