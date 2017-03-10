6f865130a06606755c2637a9c2bc12311e8f7072
Nikos Kostoulas <kostoulas@workable.com>
2017-03-10 17:19:21 +0200

    Export Processor

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import './register-routes';
 export { default as Route } from './route';
+export { default as Processor } from './processors/processor';
 export { default as Store } from './processors/aggregator/store';
 export { default as Timer } from './processors/aggregator/timer';
 export { default as AggregationStrategy } from './processors/aggregator/aggregation-strategy';
