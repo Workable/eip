1af1804eb1ae96b486ffde5a00efb3702a96d71b
Nikolaos Dimos <nikosd23@gmail.com>
2026-02-17 18:05:42 +0200

    Add return type to Queue.enqueue

--- src/lib/processors/throttler/queue.ts
+++ src/lib/processors/throttler/queue.ts
@@ -1,7 +1,7 @@
 import * as EventEmmiter from 'events';
 
 abstract class Queue extends EventEmmiter.EventEmitter {
-  abstract enqueue(id: string, priority: number, event: any);
+  abstract enqueue(id: string, priority: number, event: any): void | Promise<void>;
 
   abstract dequeue(): Promise<any[]>;
 }
