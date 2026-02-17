1bbf0cd1e412e16fad143b242f01cc7791a69ef7
Nikolaos Dimos <nikosd23@gmail.com>
2026-02-17 18:03:22 +0200

    Add return type to AggregationStrategy.process

--- src/lib/processors/aggregator/aggregation-strategy.ts
+++ src/lib/processors/aggregator/aggregation-strategy.ts
@@ -2,7 +2,7 @@ import * as EventEmmiter from 'events';
 import Store from './store';
 
 abstract class AggregationStrategy extends EventEmmiter.EventEmitter {
-  abstract process(event);
+  abstract process(event): void | Promise<void>;
 
   inject(event, status = Store.STATUS.COMPLETED) {
     this.emit('event', event, status);
